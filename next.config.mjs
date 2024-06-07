251944c191422e3fd041e79753780cf81ac9890b
Comstock006 <hamhoevnazir0695@gmail.com>
2024-06-08 01:50:58 +0300

    zoomwork

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    env: {
+        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+      },
+};
 
 export default nextConfig;
+
